50d6e9edfbf5ac30ce87125f3b5b7602cac1ebd0
Umang375 <ujain7315@gmail.com>
2022-11-06 00:13:47 +0530

    added delete and edit icons

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -11,7 +11,7 @@ const Notes = () => {
       <h2>Your Notes</h2>
 
       {notes.map((note) => {
-        return <NoteItem note={note} />;
+        return <NoteItem key={note._id} note={note} />;
       })}
     </div>
   );
--- src/context/NoteState.js
+++ src/context/NoteState.js
@@ -6,7 +6,7 @@ const NoteState = (props) => {
     const notesInital=[
         
             {
-              "_id": "6363ee2dd369c931e47e98d6",
+              "_id": "6363eeg2dd369c931e47e98d6",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd updation tv",
               "description": "all (this too updated have hd tv",
@@ -15,7 +15,7 @@ const NoteState = (props) => {
               "__v": 0
             },
             {
-              "_id": "63668e2c4257880f6d64d27c",
+              "_id": "63668e2t4257880f6d64d27c",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd tv",
               "description": "all  ghfghf ghfh have hd tv",
@@ -24,7 +24,7 @@ const NoteState = (props) => {
               "__v": 0
             },
             {
-              "_id": "63668e2f4257880f6d64d27e",
+              "_id": "63668e2f4s257880f6d64d27e",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd kl hjghj tv",
               "description": "all  ghfghf ghfh have hd tv",
@@ -33,7 +33,7 @@ const NoteState = (props) => {
               "__v": 0
             },
             {
-              "_id": "63668e2f4257880f6d64d280",
+              "_id": "63668e2f425a7880f6d64d280",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd kl hjghj tv",
               "description": "all  ghfghf ghfh have hd tv",
@@ -42,7 +42,7 @@ const NoteState = (props) => {
               "__v": 0
             },
             {
-              "_id": "63668e304257880f6d64d282",
+              "_id": "63668e30425h7880f6d64d282",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd kl hjghj tv",
               "description": "all  ghfghf ghfh have hd tv",
@@ -51,7 +51,7 @@ const NoteState = (props) => {
               "__v": 0
             },
             {
-              "_id": "63668e474257880f6d64d284",
+              "_id": "63668e47b4257880f6d64d284",
               "user": "635c38ec55cd5c2420f33322",
               "title": "hd kl hjghj tv",
               "description": "all  ghfghf ghfh have hd tv",
